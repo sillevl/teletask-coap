d2eaf6197f28fd542ad6b4877db97074cdc5a0aa
Sille Van Landschoot <info@sillevl.be>
2015-11-21 20:42:31 +0100

    working application with modified finalhandler module

--- app.js
+++ app.js
@@ -1,34 +1,42 @@
-var coapi = require('./teletask-coap'),
-    Teletask  = require('node-teletask'),
-    config    = require('./teletask.json')
+var finalhandler = require('finalhandler')
+var coap         = require('coap')
+var Router       = require('router')
+var Teletask     = require('node-teletask')
+var config       = require('./teletask.json')
+var linkFormat   = require('./lib/link-format')
 
-var app = coapi();
+var router = Router()
 var teletask = new Teletask.connect(config.host,config.port);
 
-app.get('/', function (req, res) {
-  res.end('Hello World!');
+router.get('/', function (req, res) {
+  res.end('Teletask coap api');
 });
 
 // set the default content format responses
-app.use(function(req, res){
+router.use(function(req, res, next){
   res.setOption("Content-Format", "application/json");
-})
+  next()
+});
 
 // enable CoAP service discovery using /.well-know uri
-app.get("/.well-known/core", function(req, res){
+router.get("/.well-known/core", function(req, res){
   res.setOption("Content-Format", "application/link-format");
-  res.end(buildCoreFormat(config))
-})
+  res.end(linkFormat.build(config));
+});
 
 // manage relay resources
-app.route("/relay/:number")
+router.route("/relay/:number")
   .get(function(req, res){
-    teletask.get(Teletask.functions.relay, number, function(data){
-      res.end(JSON.stringify(data.value))
+    teletask.get(Teletask.functions.relay, req.params.number, function(data){
+      res.end(JSON.stringify(data.value));
 		});
   })
   .post(function(req, res){
+    teletask.set(Teletask.functions.relay, req.params.number, 103);
+  });
 
-  })
+var server = coap.createServer(function(req, res) {
+  router(req, res, finalhandler(req, res))
+})
 
-var server = app.listen();
+server.listen()
